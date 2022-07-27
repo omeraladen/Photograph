44d6420559ada23f618c228b4a864fe79069503e
omeraladen <killerpop1738@gmail.com>
2022-07-27 18:20:00 +0200

    working in logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle('window-scrolled'), window.scrollY > 0;
+    document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
 })
 
 // let nav = document.querySelector('nav');
