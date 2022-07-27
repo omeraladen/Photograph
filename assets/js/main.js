4a709b952c15262bfaf33068c1770703e2a7b84e
omeraladen <killerpop1738@gmail.com>
2022-07-27 14:38:31 +0200

    finsh part 2 of nav with scroll featcher

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scrolled'), window.scrollY > 0;
+})
+
+// let nav = document.querySelector('nav');
+// window.onscroll = function(){
+//     if(document.documentElement.scrollTop > 0){
+//         nav.classList.add("window-scrolled");
+//     }else{
+//         nav.classList.remove("window-scrolled");
+//     }
+// }
