322099b0b8f030274b32fe49650302c4ac3970d3
omeraladen <killerpop1738@gmail.com>
2022-07-28 17:34:53 +0200

    Finsh Amazing navbar from dribble design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,14 @@
+// Active NavBar
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
 })
 
-// let nav = document.querySelector('nav');
-// window.onscroll = function(){
-//     if(document.documentElement.scrollTop > 0){
-//         nav.classList.add("window-scrolled");
-//     }else{
-//         nav.classList.remove("window-scrolled");
-//     }
-// }
+// Contact Button ( Circular text around button )
+const textButtons = document.querySelectorAll('.contact__btn');
+
+textButtons.forEach(textButtons => {
+    let text = textButtons.querySelector('p');
+
+    text.innerHTML = text.innerHTML.split('').map
+    ((character, index) => `<span style="transform: rotate(${index * 12}deg)"> ${character} </span>`).join('')
+})
