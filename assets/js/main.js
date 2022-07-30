d671bd5611ffa424023b784b5ded53f8c46a3409
omeraladen <killerpop1738@gmail.com>
2022-07-30 23:50:23 +0200

    finish gallery Section with Swiper.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,23 @@ textButtons.forEach(textButtons => {
     text.innerHTML = text.innerHTML.split('').map
     ((character, index) => `<span style="transform: rotate(${index * 12}deg)"> ${character} </span>`).join('')
 })
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView:1,
+    spaceBetween:20, // mobile view small Screen
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,      
+    },
+    breakpoints:{
+        599: {
+            slidesPerView:2, // on Mobile Layout => Tablet view
+            spaceBetween:40
+        },
+        1023:{
+            slidesPerView:3,
+            spaceBetween:60
+        }
+    }
+  });s 
